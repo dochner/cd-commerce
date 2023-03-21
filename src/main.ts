b9e341036aeb03b611464f1bc34e7445ef9aa7b7
Douglas Ochner <douglas.ochner@gmail.com>
2023-03-21 21:08:31 +0000

    feat: add auto-import plugins and nprogress plugin

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-
 import { createRouter, createWebHistory } from 'vue-router'
 import { UserModule } from './types'
 
+import './styles/main.css'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('./pages/index.vue'),
+  },
+  {
+    path: '/about',
+    component: () => import('./pages/about.vue'),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () => import('./pages/index.vue'),
-    },
-  ]
+  routes,
 })
 
 const app = createApp(App)
 app.use(router)
+
+Object.values(
+  import.meta.glob<{ install: UserModule }>("./plugins/*.{js,ts}", { eager: true })
+).forEach((i) => i.install?.({ app, router }));;
 
 app.mount('#app')
---
+++ src/pages/about.vue
@@ -0,0 +1,10 @@
+
+<script setup>
+import HelloWorld from '~/components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>
+    <HelloWorld msg="About" />
+  </div>
+</template>
