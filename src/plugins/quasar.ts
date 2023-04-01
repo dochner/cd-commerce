5b313208e7d4c86516950d660e35247a198fe7ef
Douglas Ochner <douglas.ochner@gmail.com>
2023-04-01 13:29:37 +0100

    refactor(prettier): prettify quasar config file

--- src/plugins/quasar.ts
+++ src/plugins/quasar.ts
@@ -1,30 +1,28 @@
-import { UserModule } from '~/types'
-import { Quasar, Notify, QuasarPluginOptions } from 'quasar'
+import { UserModule } from "~/types";
+import { Quasar, Notify, QuasarPluginOptions } from "quasar";
 
-import 'quasar/src/css/index.sass'
-import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/roboto-font/roboto-font.css'
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
+import "@quasar/extras/roboto-font/roboto-font.css";
 
 export const install: UserModule = ({ app }) => {
   app.use(Quasar, {
     config: {
       dark: true,
       brand: {
-        primary: '#3f59f2',
-        secondary: '#9900f1',
-        accent: '#1f3eeb',
-        dark: '#1D1D1D',
-        positive: '#21BA45',
-        negative: '#C10015',
-        info: '#31CCEC',
-        warning: '#F2C037',
+        primary: "#3f59f2",
+        secondary: "#9900f1",
+        accent: "#1f3eeb",
+        dark: "#1D1D1D",
+        positive: "#21BA45",
+        negative: "#C10015",
+        info: "#31CCEC",
+        warning: "#F2C037",
       },
-      extras: [
-        'roboto-font',
-      ],
+      extras: ["roboto-font"],
     },
     plugins: {
       Notify,
     },
-  } as Partial<QuasarPluginOptions>)
-}
+  } as Partial<QuasarPluginOptions>);
+};
