b9b572ad661a483c37cd14a0deeca65cdd6b1b22
Douglas Ochner <douglas.ochner@gmail.com>
2023-04-01 13:02:03 +0100

    feat(lint): add eslint and prettier configs and packages

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: { parser: "@typescript-eslint/parser" },
+  rules: {
+    "vue/require-default-prop": "off",
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
