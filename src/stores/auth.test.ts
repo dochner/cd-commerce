104bd2bee68aae77975ef076d60e7f353baf7967
Douglas Ochner <douglas.ochner@gmail.com>
2023-04-17 11:14:47 +0100

    feat: add pinia and simple auth store test with pinia mock

--- src/stores/auth.test.ts
+++ src/stores/auth.test.ts
@@ -1,16 +1,36 @@
-import { beforeAll, expect } from 'vitest';
+import { useUserStore } from './user';
+import { UserSignIn } from './../types/index';
+import { expect } from 'vitest';
 import { describe, test } from 'vitest'
 import { useAuthStore } from './auth'
-import {setActivePinia, createPinia} from 'pinia'
 
 describe('useAuthStore', () => {
-  beforeAll(() => {
-    setActivePinia(createPinia())
-  })
-
   test('should be equal to userStore', () => {
     const authStore = useAuthStore()
 
-    expect(authStore.userCredentials.email).not.toEqual("")
+    expect(authStore.userCredentials.email).toEqual("")
+  })
+
+  test('should set credentials value', () => {
+    const authStore = useAuthStore()
+
+    const userCredentials: UserSignIn = { email: 'example@example.com', password: '123' }
+    
+    authStore.setUserCredentials(userCredentials)
+    expect(authStore.userCredentials.email).toEqual(userCredentials.email)
+    expect(authStore.userCredentials.password).toEqual(userCredentials.password)
+  })
+
+  test('should set userStore user property', async () => {
+    const authStore = useAuthStore()
+    const userStore = useUserStore()
+
+    const userCredentials: UserSignIn = { email: 'example@example.com', password: '123' }
+    
+    authStore.setUserCredentials(userCredentials)
+
+    await authStore.signIn()
+    
+    expect(userStore.user?.email).toEqual(userCredentials.email)
   })
 })
